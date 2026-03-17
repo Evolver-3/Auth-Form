772ca4d1b0008308945809dd1405e107256c1b81
Ashu Singh <Ashusingh2201@gmail.com>
2026-03-17 23:40:40 +0530

    running of app with database

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv'
+
+dotenv.config({
+  path:"./.env"
+})
+
+import { app } from './src/app.js'
+import { connectDB } from './src/database/db.js'
+
+connectDB().then(()=>{
+  app.listen(process.env.PORT  || 8000,()=>{
+    console.log(`Server is running on port ${process.env.PORT}`)
+  })
+}).catch((error)=>{
+  console.error("failed to connect to the database:",error)
+})
