f16007417159fc7646e3dca6e7f181a7187f796d
Ashu Singh <Ashusingh2201@gmail.com>
2026-03-18 23:05:50 +0530

    removed dotenv and added a import of it to fix the CORS error

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,7 @@
-import dotenv from 'dotenv'
-
-dotenv.config({
-  path:"./.env"
-})
+import './src/config/config.js'
 
 import { app } from './src/app.js'
+
 import { connectDB } from './src/database/db.js'
 
 connectDB().then(()=>{
