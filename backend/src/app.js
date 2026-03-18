f2c91393d695413de86a1c47210a14a3b982e5de
Ashu Singh <Ashusingh2201@gmail.com>
2026-03-18 23:05:05 +0530

    added path of CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,10 +5,11 @@ import cors from 'cors'
 const app=express()
 
 app.use(cors({
-  origin:process.env.CORS,
+  origin:process.env.CORS || 'http://localhost:5173',
   credentials:true
 }))
 
+ console.log("CORS value:", process.env.CORS)
 
 app.use(express.json({
   limit:"16kb"
@@ -23,7 +24,10 @@ app.use(express.urlencoded({
 app.use(cookieParser())
 
 
+
+
 import routes from './routes/user.routes.js'
 
 app.use("/api/v1/users",routes)
+
 export {app}
