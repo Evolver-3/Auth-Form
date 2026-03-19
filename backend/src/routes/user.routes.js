663111415cabaa9f020417d9e06f9346b9c72364
Ashu Singh <Ashusingh2201@gmail.com>
2026-03-20 00:43:10 +0530

    routes added for userProfile

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 
-import {loginUser, registerUser,logoutUser, refreshAccessToken} from '../controllers/user.controller.js'
+import {loginUser, registerUser,logoutUser, refreshAccessToken, getUserProfile} from '../controllers/user.controller.js'
 import { verifyJWT } from "../middleware/auth.middleware.js";
+
 
 const router=Router()
 
@@ -12,5 +13,7 @@ router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJWT,logoutUser)
 
 router.route("/refreshToken").post(refreshAccessToken)
+
+router.route("/profile").get(verifyJWT,getUserProfile)
 
 export default router
