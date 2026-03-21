c73820ec489b35995837af57668255ede532efbd
Ashu Singh <Ashusingh2201@gmail.com>
2026-03-21 22:05:49 +0530

    refactor: replace AuthenticationPage with AuthProvider and RouterProvider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,15 @@
-import AuthenticationPage from "./form/AuthenticationPage"
+import { RouterProvider } from "react-router-dom"
+import { router } from "./app.routes.jsx"
+import { AuthProvider } from "./features/auth/auth.context.jsx"
 const App = () => {
   return (
-    <div>
-      <AuthenticationPage/>
-    </div>
+    
+    
+      <AuthProvider>
+        <RouterProvider router={router}/>
+      </AuthProvider>
+
+   
   )
 }
 
