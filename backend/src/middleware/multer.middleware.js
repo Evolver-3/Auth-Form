d555fb41e207166c642db26420ef2c7446dd9f7a
Ashu Singh <Ashusingh2201@gmail.com>
2026-03-23 01:05:26 +0530

    refactor multer middleware to use memory storage and set file size limit

--- backend/src/middleware/multer.middleware.js
+++ backend/src/middleware/multer.middleware.js
@@ -1,12 +1,11 @@
 import multer from 'multer'
 
-const storage=multer.diskStorage({
-  destination:function(req,file,cb){
-    cb(null,"./public/temp")
-  },
-  filename:function(req,file,cb){
-    cb(null,file.originalname)
+
+const upload=multer({
+  storage:multer.memoryStorage(),
+  limits:{
+    fileSize:3 * 1024 *1024
   }
 })
 
-export const upload=multer({storage})
+export { upload }
