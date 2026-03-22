81fbf049700d505a8a2a17cf6f0e6f5dc0aff88d
Ashu Singh <Ashusingh2201@gmail.com>
2026-03-23 01:03:43 +0530

    add interview report model with schemas for technical, behavioral questions, skills, and preparation

---
+++ backend/src/models/interviewReport.models.js
@@ -0,0 +1,109 @@
+import mongoose from 'mongoose'
+
+const technicalQuestionSchema=new mongoose.Schema({
+  question:{
+    type:String,
+    required:[true,"question is required"]
+  },
+  intention:{
+    type:String,
+    required:[true,"intention is required"]
+  },
+  answer:{
+    type:String,
+    required:[true,"answer is required"]
+  }
+},
+{
+  _id:false
+})
+
+const behavioralQuestionSchema=new mongoose.Schema({
+  question:{
+    type:String,
+    required:[true,"question is required"]
+  },
+  intention:{
+    type:String,
+    required:[true,"intention is required"]
+  },
+  answer:{
+    type:String,
+    required:[true,"answer is required"]
+  }
+},
+{
+  _id:false
+})
+
+const skillSchema=new mongoose.Schema({
+  skill:{
+    type:String,
+    required:[true,"skill is required"]
+  },
+  severity:{
+    type:String,
+    enum:["low","medium","high"],
+    required:[true,"severity is required"]
+  }
+},
+{
+  _id:false
+})
+
+const preparationSchema=new mongoose.Schema(
+  {
+    day:{
+      type:Number,
+      required:[true,"day is required"]
+    },
+    focus:{
+      type:String,
+      required:[true,'focus is required']
+    },
+    tasks:[
+      {
+        type:String,
+        required:[true,'task is required']
+      }
+    ]
+
+  }
+)
+
+const interviewReportSchema=new mongoose.Schema(
+  {
+    jobDescription:{
+      type:String,
+      required:[true,"job description is required"]
+    },
+    resume:{
+      type:String
+    },
+    selfDescription:{
+      type:String
+    },
+    matchScore:{
+      type:Number,
+      min:0,
+      max:100
+    },
+    technicalQuestions:[technicalQuestionSchema],
+
+    behavioralQuestions:[behavioralQuestionSchema],
+
+    skills:[skillSchema],
+
+    preparation:[preparationSchema],
+    user:{
+      type:mongoose.Schema.Types.ObjectId,
+      ref:"User"
+    }
+
+  },
+  {
+    timestamps:true
+  }
+)
+
+export const InterviewReport=mongoose.model("InterviewReport",interviewReportSchema)
