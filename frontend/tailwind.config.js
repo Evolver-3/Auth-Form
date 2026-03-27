daa572d4a8c6781a4e0485bb00b60ca5ab0399f1
Ashu Singh <Ashusingh2201@gmail.com>
2026-03-27 21:47:55 +0530

    added some custom properties which can be used anywhere

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "oklch(58.6% 0.253 17.585)",
+        secondary: "oklch(62.7% 0.265 303.9)",
+        third: "oklch(54.1% 0.281 293.009)",
+      },
+      fontSize: {
+        first: "40px",
+        sec: "20px",
+        triple:"16px"
+      },
+      boxShadow: {
+        soft: "0 4px 10px rgba(0, 0, 0, 0.15)",
+        strong: "0 1px 4px oklch(44.6% 0.043 257.281)",
+        glow: "0 5px 20px oklch(37.3% 0.034 259.733)",
+        weird:
+          "0px 4px 20px 0px rgba(15,12,12,0.08),0px 8px 12px -4px rgba(15,12,12,0.10),0px 0px 2px 0px rgba(15,12,12,0.10),0px 1px 2px 0px rgba(15,12,12,0.10)",
+        finta:
+          "0px 8px 12px -4px rgba(15,12,12,0.08),0px 0px 2px 0px rgba(15,12,12,0.10),0px 1px 2px 0px rgba(15,12,12,0.10)",
+      },
+    },
   },
   plugins: [],
 }
-
