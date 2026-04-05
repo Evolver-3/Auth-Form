641240e6d1a7bfac16359297fb6dc20fe052dcc8
Ashu Singh <Ashusingh2201@gmail.com>
2026-04-05 23:47:07 +0530

    remove unused user profile fetching logic from AuthProvider

--- frontend/src/features/auth/auth.context.jsx
+++ frontend/src/features/auth/auth.context.jsx
@@ -1,5 +1,5 @@
-import { createContext,useEffect,useState } from "react";
-import { profile } from "./services/auth.api";
+import { createContext,useState } from "react";
+
 
 export const AuthContext=createContext()
 
@@ -8,25 +8,6 @@ export const AuthProvider=({children})=>{
 
   const [user,setUser]=useState(null)
   const [loading,setLoading]=useState(true)
-
-  useEffect(()=>{
-    const getAndSetUser=async()=>{
-      try {
-        const data=await profile()
-        setUser(data.user)
-        
-      } catch (error) {
-        console.error("Error fetching user profile:",error)
-        setUser(null)
-        
-      }finally{
-        setLoading(false)
-      }
-    }
-    getAndSetUser()
-  },[])
-
-
 
   return (
     <AuthContext.Provider value={{user,setUser,loading,setLoading}}>
