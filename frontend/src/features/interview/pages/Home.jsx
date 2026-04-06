548e5b37c5809281ae87f4edbd5f0b3c8d44b793
Ashu Singh <Ashusingh2201@gmail.com>
2026-04-06 23:57:32 +0530

    updated UI

--- frontend/src/features/interview/pages/Home.jsx
+++ frontend/src/features/interview/pages/Home.jsx
@@ -9,14 +9,18 @@ const Home=()=>{
 
   const [jobDescription,setJobDescription]=useState("")
   const [selfDescription,setSelfDescription]=useState("")
-  const resumeRef=useRef()
+
+  const [resumeUploaded,setResumeUploaded]=useState(null)
+
 
   const navigate=useNavigate()
 
   const handleGenerateReport=async()=>{
-    const resumeFile=resumeRef.current.files[0]
+  
 
-    const data=await generateReport({jobDescription,selfDescription,resume:resumeFile})
+    const data=await generateReport({jobDescription,selfDescription,resume:resumeUploaded})
+
+    console.log(data)
    
 
     navigate(`/report/${data._id}`)
@@ -35,35 +39,59 @@ const Home=()=>{
 
   return(
     <HomePageWrapper>
-    <div className="h-screen flex flex-col items-center justify-center gap-5 ">
+    <div className="alignDiv px-4 md:px-20 lg:px-60">
 
-      <div className=" border border-netrual-100 rounded-md px-5 py-5 flex items-center justify-center gap-5 ">
-        <label htmlFor="JobDescription" >Job Description</label>
-        <textarea
-        onChange={(e)=>(setJobDescription(e.target.value))}
-        name="jobDesciption" placeholder="Enter job description here" id="jobDescription"></textarea>
-      </div>
+      <h2 className='h2Headings'>Check your previous Reports</h2>
 
-      <div className='border border-neutral-100 rounded-md  flex flex-col items-center justify-center gap-2 px-5 py-5'>
-        <div className="flex items-center gap-5 ">
-          <label htmlFor="resume" >Upload Resume</label>
-          <input hidden ref={resumeRef}  type="file" name='resume' id='resume' accept=".pdf" className="border hover:outline-none bg-rose-200"></input>
-        </div>
+      <div className='w-full flex flex-col py-10 px-4 items-center justify-between gap-8 rounded-md shadow-weird bg-white'>
 
-        <div className="flex items-center gap-5">
-          <label htmlFor='selfDesciption'>Self Description</label>
-          <textarea 
-          onChange={(e)=>(setSelfDescription(e.target.value))}
-          className="" name="selfDescription" placeholder="Enter your self Description here" id="selfDescription"></textarea>
-        </div>
+
+        <LabelData text={"selfDescription"} textData={"Self Description"} onChange={(e)=>{setSelfDescription(e.target.value)}} placeholder={"Enter About Yourself"}/>
+
+        <LabelData text={"jobDescription"} textData={"Job Description"} onChange={(e)=>{setJobDescription(e.target.value)}} placeholder={"Enter aboute which job are you targeting"}/>
+     
+        <label htmlFor="resume" className='flex gap-3 items-center'>
+          <span className={`${resumeUploaded ? "text-green-500": "text-red-500"}`}><SvgMedia/></span>
+          <h2 className='text-md text-neutral-600 font-semibold'>Upload Resume</h2>
+        </label>
+        <input hidden onChange={(e)=>setResumeUploaded(e.target.files[0])}  type="file" name='resume' id='resume' accept=".pdf" className="border hover:outline-none"></input>
 
         <button
         onClick={handleGenerateReport}
-        className="btn">Generate Interview Report</button>
+        className="px-4 py-1.5">Generate Interview Report
+        </button>
+
       </div>
+   
+
     </div>
+ 
     </HomePageWrapper>
   )
 }
 
 export default Home
+
+const LabelData=({text,textData,onChange,placeholder})=>{
+  return (
+    <div className='flex items-center justify-around w-full '>
+      <label className=' text-md md:text-lg lg:text-xl text-neutral-600 font-semibold ' htmlFor={text}>{textData}</label>
+
+      <textarea 
+      className='w-80 lg:w-2/3  bg-neutral-100 rounded-md p-3 h-[100px] ring-1 ring-rose-200 outline-none text-neutral-500 text-sm resize-none hover:shadow-soft hover:shadow-rose-200  transition-colors duration-150 ease-in-out placeholder:hover:opacity-0'
+      onChange={onChange}
+      name={text}
+      placeholder={placeholder}
+      />
+    </div>
+  )
+}
+
+const SvgMedia=()=>{
+  return(
+    <svg  xmlns="http://www.w3.org/2000/svg" width="24" height="24"  
+    fill="currentColor" viewBox="0 0 24 24" >
+      <path d="M19 3h-2c0-.55-.45-1-1-1H8c-.55 0-1 .45-1 1H5c-1.1 0-2 .9-2 2v15c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2m0 17H5V5h2v2h10V5h2z"></path><path d="M11 14.09 8.71 11.8 7.3 13.21l3 3c.2.2.45.29.71.29s.51-.1.71-.29l5-5-1.41-1.41-4.29 4.29Z"></path>
+    </svg>
+  )
+}
