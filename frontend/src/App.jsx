2d1df9264fd6ca1bdbf6fdc4fc23e7cd99921c68
Ashu Singh <Ashusingh2201@gmail.com>
2026-04-09 18:13:38 +0530

    all additional changes with package details

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
 import { RouterProvider } from "react-router-dom"
 import { router } from "./app.routes.jsx"
 import { AuthProvider } from "./features/auth/auth.context.jsx"
+import { InterviewProvider } from "./features/interview/interview.context.jsx"
 const App = () => {
   return (
     
     
       <AuthProvider>
-        <RouterProvider router={router}/>
+        <InterviewProvider>
+          <RouterProvider router={router}/>
+        </InterviewProvider>
       </AuthProvider>
+      
 
    
   )
