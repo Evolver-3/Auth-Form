af4be6e88db48682840692049b61c09150f59f4a
Ashu Singh <Ashusingh2201@gmail.com>
2026-05-17 18:15:38 +0530

    for mobile screen remove the padding from the login and register page

--- frontend/src/features/auth/pages/FrontPageComponent.jsx
+++ frontend/src/features/auth/pages/FrontPageComponent.jsx
@@ -3,15 +3,15 @@ import { Link } from "react-router-dom"
 export const FrontPageComponent=({children,text,textspan,point})=>{
   return(
     <main >
-      <div className=" flex h-screen px-5 py-4 ">
+      <div className=" flex h-screen md:px-5 md:py-4 ">
    
-        <div className=" w-[60%] py-1 bg-slate-200 shadow-weird rounded-l-xl ">
+        <div className=" w-[60%] py-1 bg-slate-200 shadow-weird md:rounded-l-xl ">
           {children}
           <p className="text-[12px] md:text-[15px] text-center ">{text} <Link
           className="text-blue-600 hover:underline hover:underline-offset-2 transform-gpu duration-200 focus:scale-[101%]" to={point}>{textspan}</Link></p>
         </div>
     
-        <div className="w-[40%]  relative bg-neutral-300 z-0 group  overflow-hidden hover:bg-neutral-400 transform-cpu duration-300 rounded-r-xl shadow-weird ">
+        <div className="w-[40%]  relative bg-neutral-300 z-0 group  overflow-hidden hover:bg-neutral-400 transform-cpu duration-300 md:rounded-r-xl shadow-weird ">
 
           <div className="w-full absolute inset-0 bg-[url('/Cover.jpg')] bg-cover bg-center bg-no-repeat opacity-80 group-hover:scale-110 transform-gpu duration-300 hover:blur-sm"></div>
 
